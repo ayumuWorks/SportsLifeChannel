94f2e451cbf73d588e7d6027fd36adbef2f28c1b
津田歩武 <ayumu@tsudahobu-no-MacBook-Pro.local>
2015-12-20 16:56:05 +0900

    first commit

---
+++ channel01.php
@@ -0,0 +1,117 @@
+<?php
+
+	include 'header.php';
+
+?>
+
+	
+	<div id="channel01-01" class="uk-container uk-container-center">
+		<h2 class="white-panel tit">スポーツライフチャンネル</h2>
+	</div>
+
+	<div id="channel01-02">
+		<h3 class="blue-panel tit">夢の対談チャンネル</h3>
+		<div class="uk-container uk-container-center">
+			<div class="uk-grid uk-grid-width-small-1-2 uk-grid-width-large-1-3 clear" data-uk-grid-margin>
+				<section class="uk-scrollspy-init-inview com-panel-02" data-uk-scrollspy="{cls:'uk-animation-slide-bottom', repeat: true}">
+					<div class="img">
+						<a href="profile-01.php">
+							<img src="images/channel01/01_02.png">	
+							<div class="name">
+								<p>立命館大学</p>
+								<h2>津田 歩武</h2>
+							</div>
+						</a>
+					</div>
+				</section>
+				<section class="uk-scrollspy-init-inview com-panel-02" data-uk-scrollspy="{cls:'uk-animation-slide-bottom', repeat: true}">
+					<a href="profile-01.php">
+						<img src="images/channel01/01_03.png">	
+						<div class="name">
+							<p>立命館大学</p>
+							<h2>津田 歩武</h2>
+						</div>
+					</a>
+				</section>
+				<section class="uk-hidden-small uk-scrollspy-init-inview com-panel-02" data-uk-scrollspy="{cls:'uk-animation-slide-bottom', repeat: true}">
+					<a href="profile-01.php">
+						<img src="images/channel01/01_04.png">	
+						<div class="name">
+							<p>立命館大学</p>
+							<h2>津田 歩武</h2>
+						</div>
+					</a>
+				</section>
+			
+				<section class="uk-scrollspy-init-inview com-panel-02" data-uk-scrollspy="{cls:'uk-animation-slide-bottom', repeat: true}">
+					<div class="img">
+						<a href="profile-01.php">
+							<img src="images/channel01/01_05.png">	
+							<div class="name">
+								<p>立命館大学</p>
+								<h2>津田 歩武</h2>
+							</div>
+						</a>
+					</div>
+				</section>
+				<section class="uk-scrollspy-init-inview com-panel-02" data-uk-scrollspy="{cls:'uk-animation-slide-bottom', repeat: true}">
+					<a href="profile-01.php">
+						<img src="images/channel01/01_06.png">	
+						<div class="name">
+							<p>立命館大学</p>
+							<h2>津田 歩武</h2>
+						</div>
+					</a>
+				</section>
+				<section class="uk-hidden-small uk-scrollspy-init-inview com-panel-02" data-uk-scrollspy="{cls:'uk-animation-slide-bottom', repeat: true}">
+					<a href="profile-01.php">
+						<img src="images/channel01/01_07.png">	
+						<div class="name">
+							<p>立命館大学</p>
+							<h2>津田 歩武</h2>
+						</div>
+					</a>
+				</section>
+			
+				<section class="uk-scrollspy-init-inview com-panel-02" data-uk-scrollspy="{cls:'uk-animation-slide-bottom', repeat: true}">
+					<div class="img">
+						<a href="profile-01.php">
+							<img src="images/channel01/01_09.png">	
+							<div class="name">
+								<p>立命館大学</p>
+								<h2>津田 歩武</h2>
+							</div>
+						</a>
+					</div>
+				</section>
+				<section class="uk-scrollspy-init-inview com-panel-02" data-uk-scrollspy="{cls:'uk-animation-slide-bottom', repeat: true}">
+					<a href="profile-01.php">
+						<img src="images/channel01/01_03.png">	
+						<div class="name">
+							<p>立命館大学</p>
+							<h2>津田 歩武</h2>
+						</div>
+					</a>
+				</section>
+				<section class="uk-hidden-small uk-scrollspy-init-inview com-panel-02" data-uk-scrollspy="{cls:'uk-animation-slide-bottom', repeat: true}">
+					<a href="profile-01.php">
+						<img src="images/channel01/01_04.png">	
+						<div class="name">
+							<p>立命館大学</p>
+							<h2>津田 歩武</h2>
+						</div>
+					</a>
+				</section>
+			</div>
+		</div>
+	</div>
+				
+	</div>
+
+	
+
+<?php
+
+	include 'footer.php';
+
+?>
